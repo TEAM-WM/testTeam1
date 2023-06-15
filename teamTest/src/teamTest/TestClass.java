d608fcc9516ea3fbb93a6e285dd746e8da9cdebd
hyuzzi <codex1019@gmail.com>
2023-06-15 20:35:31 +0900

    Feat : test liyeon

--- teamTest/src/teamTest/TestClass.java
+++ teamTest/src/teamTest/TestClass.java
@@ -4,5 +4,6 @@ public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("테스트");
 		System.out.println("테스트리연");
+		System.out.println("테스트리연222222");
 	}//main
 }//class
